181c69c400d5ba68fad4a18846ae6d22e3633f33
ander <soer9428@edu.easj.dk>
2021-09-07 11:00:35 +0200

    setup af html og lidt css.

--- golfscore.php
+++ golfscore.php
@@ -2,16 +2,26 @@
 <html lang="da">
 <head>
     <meta charset="utf-8">
-    <title>Sigende titel</title>
+    <title>Golf Score</title>
     <meta name="robots" content="All">
-    <meta name="author" content="Udgiver">
-    <meta name="copyright" content="Information om copyright">
+    <meta name="author" content="Søren Andersen">
+    <meta name="copyright" content="Søren Andersen">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 
+<h2 class="text-center my-5">Golf Score</h2>
+
+<div class="container d-flex justify-content-center my-5">
+    <div class="inputfield">
+        <button class="btn__style"><i class="fa-solid fa-arrow-down"></i></button>
+            <input placeholder="" class="score">
+        <button class="btn__style"><i class="fa-solid fa-arrow-up"></i></button>
+    </div>
+</div>
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
