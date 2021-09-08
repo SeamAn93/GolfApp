a33ce299a50a07c161dd05764776c48d6890831a
ander <soer9428@edu.easj.dk>
2021-09-08 21:09:47 +0200

    Opsætning af ny java og html, der virker --> FEDT Styling af selve app.

--- golfscore.php
+++ golfscore.php
@@ -10,22 +10,57 @@
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-<body>
-
-<h2 class="text-center my-5">Golf Score</h2>
-
-<div class="container d-flex justify-content-center my-5">
-    <div class="d-flex">
-        <button class="counterminus">-</button>
-        <h1 class="counterdisplay">(..)</h1>
-        <button class="counterplus">+</button>
+<body class="bg-gul">
+    <h1 class="text-center mb-4 pt-2 fw-bold">Golf score</h1>
+    <p class="text-center mb-1 fs-5">Hul 1</p>
+    <div class="inputfield d-flex justify-content-center mb-3">
+        <button class="inputminus rounded-3 border-black bg-danger text-white text-center fs-3 me-3">-</button>
+        <input class="score bg-secondary rounded-3 border-black text-center fs-3" type="text" name="input" value="0">
+        <button class="inputplus bg-success rounded-3 border-black text-white text-center fs-3 ms-3">+</button>
     </div>
-    <div class="d-flex">
-        <button class="counterminus">-</button>
-        <h1 class="counterdisplay">(..)</h1>
-        <button class="counterplus">+</button>
+    <p class="text-center mb-1 fs-5">Hul 2</p>
+    <div class="inputfield d-flex justify-content-center mb-3">
+        <button class="inputminus rounded-3 border-black bg-danger text-white text-center fs-3 me-3">-</button>
+        <input class="score bg-secondary rounded-3 border-black text-center fs-3" type="text" name="input" value="0">
+        <button class="inputplus bg-success rounded-3 border-black text-white text-center fs-3 ms-3">+</button>
     </div>
-</div>
+    <p class="text-center mb-1 fs-5">Hul 3</p>
+    <div class="inputfield d-flex justify-content-center mb-3">
+        <button class="inputminus rounded-3 border-black bg-danger text-white text-center fs-3 me-3">-</button>
+        <input class="score bg-secondary rounded-3 border-black text-center fs-3" type="text" name="input" value="0">
+        <button class="inputplus bg-success rounded-3 border-black text-white text-center fs-3 ms-3">+</button>
+    </div>
+    <p class="text-center mb-1 fs-5">Hul 4</p>
+    <div class="inputfield d-flex justify-content-center mb-3">
+        <button class="inputminus rounded-3 border-black bg-danger text-white text-center fs-3 me-3">-</button>
+        <input class="score bg-secondary rounded-3 border-black text-center fs-3" type="text" name="input" value="0">
+        <button class="inputplus bg-success rounded-3 border-black text-white text-center fs-3 ms-3">+</button>
+    </div>
+    <p class="text-center mb-1 fs-5">Hul 5</p>
+    <div class="inputfield d-flex justify-content-center mb-3">
+        <button class="inputminus rounded-3 border-black bg-danger text-white text-center fs-3 me-3">-</button>
+        <input class="score bg-secondary rounded-3 border-black text-center fs-3" type="text" name="input" value="0">
+        <button class="inputplus bg-success rounded-3 border-black text-white text-center fs-3 ms-3">+</button>
+    </div>
+    <p class="text-center mb-1 fs-5">Hul 6</p>
+    <div class="inputfield d-flex justify-content-center mb-3">
+        <button class="inputminus rounded-3 border-black bg-danger text-white text-center fs-3 me-3">-</button>
+        <input class="score bg-secondary rounded-3 border-black text-center fs-3" type="text" name="input" value="0">
+        <button class="inputplus bg-success rounded-3 border-black text-white text-center fs-3 ms-3">+</button>
+    </div>
+    <p class="text-center mb-1 fs-5">Hul 7</p>
+    <div class="inputfield d-flex justify-content-center mb-3">
+        <button class="inputminus rounded-3 border-black bg-danger text-white text-center fs-3 me-3">-</button>
+        <input class="score bg-secondary rounded-3 border-black text-center fs-3" type="text" name="input" value="0">
+        <button class="inputplus bg-success rounded-3 border-black text-white text-center fs-3 ms-3">+</button>
+    </div>
+    <h3 class="text-center">Din total score:</h3>
+    <div class="inputfield d-flex justify-content-center">
+        <input class="bg-secondary rounded-3 border-black text-center fs-3" type="text" id="total" value="0" disabled=""/>
+    </div>
+    <a href="golfscore.php" class="d-flex justify-content-center text-decoration-none my-3 mb-3">
+        <button class="bg-lilla rounded-3 border-black fs-3">Start forfra</button>
+    </a>
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <script type="module" src="js/app.js"></script>
--- js/app.js
+++ js/app.js
@@ -1,26 +1,27 @@
-let count = 0;
+let inputMinus = document.querySelectorAll('.inputminus');
+let inputPlus = document.querySelectorAll('.inputplus');
 
-let counterDisplayElem = document.querySelectorAll('.counterdisplay');
-
-let counterMinusElem = document.querySelector('.counterminus');
-
-let counterPlusElem = document.querySelector('.counterplus');
-
-
-
-
-updateDisplay();
-
-counterPlusElem.addEventListener("click",()=>{
-    count++;
-    updateDisplay();
-}) ;
-
-counterMinusElem.addEventListener("click",()=>{
-    count--;
-    updateDisplay();
+inputMinus.forEach((inputMinus) => {
+    inputMinus.addEventListener('click', function (e){
+        if(e.target.nextElementSibling.value > 0) {
+            e.target.nextElementSibling.value--}
+        getTotal();
+    });
 });
 
-function updateDisplay(){
-    counterDisplayElem.innerHTML = count;
+inputPlus.forEach((inputPlus) => {
+    inputPlus.addEventListener('click', function (e){
+        e.target.previousElementSibling.value++;
+        getTotal();
+    })
+})
+
+function getTotal(){
+    let arr = document.querySelectorAll('.score');
+    let total=0;
+    for(let i=0;i<arr.length;i++){
+        if(parseInt(arr[i].value))
+            total += parseInt(arr[i].value);
+    }
+    document.getElementById('total').value = total;
 }
