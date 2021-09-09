6c89cb470976c8b626a75a2714763d4e5396fabb
Søren <soer9428@edu.easj.dk>
2021-09-09 14:08:33 +0200

    Tilføjelse af hulnummer, så det følger antallet af huller, der valgt Tilføjelse af modal (bootstrap) og funktion "navn", så brugerens navn står øverest på app. Tilføjelse af reset btn, med javascript istedetfor

--- golfscore.php
+++ golfscore.php
@@ -11,56 +11,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body class="bg-gul">
+    <div class="d-flex justify-content-center">
+    <button type="button" class="bg-primary text-white rounded-3 border-black fs-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Start spil
+    </button>
+    </div>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Start spil</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p class="mb-1">Dit navn:</p>
+                    <input class="inputname mb-4" placeholder="Anders">
+                    <p class="mb-1">Antal huller:</p>
+                    <input class="inputholes mb-4" placeholder="5">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Luk</button>
+                    <button type="button" class="start btn btn-primary" data-bs-dismiss="modal">Start</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="makelist d-flex justify-content-center">
+        <button class="bg-primary text-white rounded-3 border-black fs-3">tilføj hul</button>
+    </div>
+
     <h1 class="text-center mb-4 pt-2 fw-bold">Golf score</h1>
-    <p class="text-center mb-1 fs-5">Hul 1</p>
-    <div class="inputfield d-flex justify-content-center mb-3">
-        <button class="inputminus rounded-3 border-black bg-danger text-white text-center fs-3 me-3">-</button>
-        <input class="score bg-secondary rounded-3 border-black text-center fs-3" type="text" name="input" value="0">
-        <button class="inputplus bg-success rounded-3 border-black text-white text-center fs-3 ms-3">+</button>
-    </div>
-    <p class="text-center mb-1 fs-5">Hul 2</p>
-    <div class="inputfield d-flex justify-content-center mb-3">
-        <button class="inputminus rounded-3 border-black bg-danger text-white text-center fs-3 me-3">-</button>
-        <input class="score bg-secondary rounded-3 border-black text-center fs-3" type="text" name="input" value="0">
-        <button class="inputplus bg-success rounded-3 border-black text-white text-center fs-3 ms-3">+</button>
-    </div>
-    <p class="text-center mb-1 fs-5">Hul 3</p>
-    <div class="inputfield d-flex justify-content-center mb-3">
-        <button class="inputminus rounded-3 border-black bg-danger text-white text-center fs-3 me-3">-</button>
-        <input class="score bg-secondary rounded-3 border-black text-center fs-3" type="text" name="input" value="0">
-        <button class="inputplus bg-success rounded-3 border-black text-white text-center fs-3 ms-3">+</button>
-    </div>
-    <p class="text-center mb-1 fs-5">Hul 4</p>
-    <div class="inputfield d-flex justify-content-center mb-3">
-        <button class="inputminus rounded-3 border-black bg-danger text-white text-center fs-3 me-3">-</button>
-        <input class="score bg-secondary rounded-3 border-black text-center fs-3" type="text" name="input" value="0">
-        <button class="inputplus bg-success rounded-3 border-black text-white text-center fs-3 ms-3">+</button>
-    </div>
-    <p class="text-center mb-1 fs-5">Hul 5</p>
-    <div class="inputfield d-flex justify-content-center mb-3">
-        <button class="inputminus rounded-3 border-black bg-danger text-white text-center fs-3 me-3">-</button>
-        <input class="score bg-secondary rounded-3 border-black text-center fs-3" type="text" name="input" value="0">
-        <button class="inputplus bg-success rounded-3 border-black text-white text-center fs-3 ms-3">+</button>
-    </div>
-    <p class="text-center mb-1 fs-5">Hul 6</p>
-    <div class="inputfield d-flex justify-content-center mb-3">
-        <button class="inputminus rounded-3 border-black bg-danger text-white text-center fs-3 me-3">-</button>
-        <input class="score bg-secondary rounded-3 border-black text-center fs-3" type="text" name="input" value="0">
-        <button class="inputplus bg-success rounded-3 border-black text-white text-center fs-3 ms-3">+</button>
-    </div>
-    <p class="text-center mb-1 fs-5">Hul 7</p>
-    <div class="inputfield d-flex justify-content-center mb-3">
-        <button class="inputminus rounded-3 border-black bg-danger text-white text-center fs-3 me-3">-</button>
-        <input class="score bg-secondary rounded-3 border-black text-center fs-3" type="text" name="input" value="0">
-        <button class="inputplus bg-success rounded-3 border-black text-white text-center fs-3 ms-3">+</button>
-    </div>
+    <h2 class="text-center mb-4 pt-2 fw-bold"><span class="name">Navn: </span></h2>
+    <ul class="list d-flex flex-column list-inline"></ul>
+
     <h3 class="text-center">Din total score:</h3>
     <div class="inputfield d-flex justify-content-center">
         <input class="bg-secondary rounded-3 border-black text-center fs-3" type="text" id="total" value="0" disabled=""/>
     </div>
-    <a href="golfscore.php" class="d-flex justify-content-center text-decoration-none my-4">
+
+    <div class="reload d-flex justify-content-center text-decoration-none my-4">
         <button class="bg-lilla rounded-3 border-black fs-3">Start forfra</button>
-    </a>
+    </div>
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <script type="module" src="js/app.js"></script>
--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 let inputMinus = document.querySelectorAll('.inputminus');
 let inputPlus = document.querySelectorAll('.inputplus');
+let holeNumber = 1;
+
+// Plus og minus
 
 inputMinus.forEach((inputMinus) => {
     inputMinus.addEventListener('click', function (e){
@@ -16,6 +19,8 @@ inputPlus.forEach((inputPlus) => {
     })
 })
 
+// Summen af huller
+
 function getTotal(){
     let arr = document.querySelectorAll('.score');
     let total=0;
@@ -24,4 +29,72 @@ function getTotal(){
             total += parseInt(arr[i].value);
     }
     document.getElementById('total').value = total;
+}
+
+// Tilføje huller
+
+const makeList = document.querySelector('.makelist');
+
+function listMaker(){
+    const li = document.createElement('li')
+    li.innerHTML = `
+            <p class="text-center mb-1 fs-5">Hul ${holeNumber}</p>
+            <div class="inputfield d-flex justify-content-center mb-3">
+                <button class="inputminus bg-danger rounded-3 border-black text-white text-center fs-3 me-3">-</button>
+                <input class="score bg-secondary rounded-3 border-black text-center fs-3" type="text" name="input" value="0">
+                <button class="inputplus bg-success rounded-3 border-black text-white text-center fs-3 ms-3">+</button>
+            </div>`;
+    const list = document.querySelector('.list');
+    list.appendChild(li);
+    li.querySelector('.inputminus').addEventListener('click', (e) =>
+        minus(e));
+    li.querySelector('.inputplus').addEventListener('click', (e) =>
+        plus(e));
+    holeNumber++;
+}
+
+makeList.addEventListener('click', (e) => {
+    listMaker(e);
+})
+
+function minus (e){
+    if(e.target.nextElementSibling.value > 0) {
+        e.target.nextElementSibling.value--}
+    getTotal();
+}
+
+function plus (e){
+    e.target.previousElementSibling.value++;
+    getTotal();
+}
+
+// Reset
+
+const reload = document.querySelector('.reload');
+
+reload.addEventListener('click',() => location.reload());
+
+// Tilføj navn
+
+let inputName = document.querySelector('.inputname');
+let nameSpan = document.querySelector('.name');
+
+function getName (){
+    const name = document.createElement('span');
+    name.innerHTML = `${inputName.value}`;
+    nameSpan.appendChild(name);
+}
+
+let startGame = document.querySelector('.start');
+
+startGame.addEventListener('click', (e) => {
+    getName();
+})
+
+// Tilføj huller
+
+let inputHoles = document.querySelector('.inputholes');
+
+function getHoles () {
+    const holes =
 }
